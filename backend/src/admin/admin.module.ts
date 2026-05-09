982aca4595887c4f4605170a7a27da04a0c42828
Tanju Baştürk <basturktanju@gmail.com>
2026-05-09 19:32:50 +0300

    fix(admin): add Availability and SpaTherapist to admin module TypeORM imports

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -4,11 +4,13 @@ import { AuthModule } from '../auth/auth.module';
 import { BookingModule } from '../booking/booking.module';
 import { MailModule } from '../mail/mail.module';
 import { RolesGuard } from '../common/guards/roles.guard';
+import { Availability } from '../database/entities/availability.entity';
 import { ClubEvent } from '../database/entities/club-event.entity';
 import { ClubEventRegistration } from '../database/entities/club-event-registration.entity';
 import { Package } from '../database/entities/package.entity';
 import { PackageType } from '../database/entities/package-type.entity';
 import { Reservation } from '../database/entities/reservation.entity';
+import { SpaTherapist } from '../database/entities/spa-therapist.entity';
 import { Trainer } from '../database/entities/trainer.entity';
 import { User } from '../database/entities/user.entity';
 import { AdminController } from './admin.controller';
@@ -27,6 +29,8 @@ import { AdminMembersService } from './admin-members.service';
       PackageType,
       Reservation,
       Trainer,
+      Availability,
+      SpaTherapist,
       ClubEvent,
       ClubEventRegistration,
     ]),
